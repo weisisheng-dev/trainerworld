5b218ef5218994b92c6b9125b7016b0a06e0056a
Vince Fulco <vfulco1@gmail.com>
2019-02-16 16:53:06 -0500

    Updates

---
+++ precache-manifest.9c27045d0abc4fd76b4693e255f553ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5152635868f5904ab23b",
+    "url": "/weisisheng-dev/trainerworld/static/js/runtime~main.51526358.js"
+  },
+  {
+    "revision": "1531464ff1b3690bbdc0",
+    "url": "/weisisheng-dev/trainerworld/static/js/main.1531464f.chunk.js"
+  },
+  {
+    "revision": "dd89dd975c1416aab287",
+    "url": "/weisisheng-dev/trainerworld/static/js/2.dd89dd97.chunk.js"
+  },
+  {
+    "revision": "1531464ff1b3690bbdc0",
+    "url": "/weisisheng-dev/trainerworld/static/css/main.c06d6352.chunk.css"
+  },
+  {
+    "revision": "dd89dd975c1416aab287",
+    "url": "/weisisheng-dev/trainerworld/static/css/2.e0ced9cd.chunk.css"
+  },
+  {
+    "revision": "18f99b91204a178288e030dbf8702d90",
+    "url": "/weisisheng-dev/trainerworld/index.html"
+  }
+];
